dcf9da760e654c368eb27c1bcb177f0402f0c43e
asha mohamed <falastine924@gmail.com>
2025-09-22 14:31:52 +0300

    groups model

--- server/models.py
+++ server/models.py
@@ -4,3 +4,19 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+#support group model
+class SupportGroup(db.Model, SerializerMixin):
+    __tablename__ = "support_groups"
+
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(120), nullable=False)
+    description= db.Column(db.Text, nullable=True)
+    meeting_times = db.Collumn(db.String(255), nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "topic": self.topic,
+            "description": self.description,
+            "meeting_times": self.meeting_times
+        }
