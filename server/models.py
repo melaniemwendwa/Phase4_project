5a2318ff61a0d6af1be295e64dae45af69bfe782
asha mohamed <falastine924@gmail.com>
2025-09-23 15:36:55 +0300

    fixing models

--- server/models.py
+++ server/models.py
@@ -4,25 +4,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
-# User model
-class User(db.Model, SerializerMixin):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String, nullable=False, unique=True)
-
-    memberships = db.relationship("Membership", back_populates="user", cascade="all, delete-orphan")
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "nickname": self.nickname,
-        }
-
-
-# SupportGroup model
+#support group model
 class SupportGroup(db.Model, SerializerMixin):
-    __tablename__ = "support_groups"
+    _tablename_ = "support_groups"
 
     id = db.Column(db.Integer, primary_key=True)
     topic = db.Column(db.String(120), nullable=False)
@@ -34,30 +18,5 @@ class SupportGroup(db.Model, SerializerMixin):
             "id": self.id,
             "topic": self.topic,
             "description": self.description,
-            "meeting_times": self.meeting_times,
-        }
-
-
-# Membership model
-class Membership(db.Model, SerializerMixin):
-    __tablename__ = "memberships"
-
-    id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.String, default="member")  # "member" or "facilitator"
-
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    support_group_id = db.Column(db.Integer, db.ForeignKey("support_groups.id"))
-
-    user = db.relationship("User", back_populates="memberships")
-    support_group = db.relationship("SupportGroup", back_populates="memberships")
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "role": self.role,
-            "user": {"id": self.user.id, "nickname": self.user.nickname},
-            "support_group": {
-                "id": self.support_group.id,
-                "topic": self.support_group.topic
-            }
+            "meeting_times": self.meeting_times
         }
